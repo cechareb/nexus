046493e8332a49256009b4ce673c192a92f68de9
Cesar Chahuas <cesar.chahuasrebatta@gmail.com>
2018-04-10 13:22:00 -0500

    Creacion de Pantalla de inicio

--- nexus/urls.py
+++ nexus/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    path('',views.inicio),
+    path('admin/', admin.site.urls),
+    path('', include('nexus_social_app.urls')),
 ]
